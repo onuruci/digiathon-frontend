fb48be47271004d6ff1ace97fc17ef99889e1980
onuruci <onur.yuce@metu.edu.tr>
2022-11-12 12:46:24 +0300

    large

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import SkyHome from "./components/SkyHome";
 import Sections from "./components/Sections";
+import './App.css';
 
 import { connectWallet, getCurrentWalletConnected } from "./utils/interaction";
 
 function App() {
   const [signer, setSigner] = useState("");
+
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+
+    return {
+      width,
+      height,
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     console.log("a");
@@ -21,10 +44,18 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <SkyHome />
-      <Sections />
-
+      {
+        windowDimensions.width >= 1400 ?
+        <>
+          <Header />
+          <SkyHome />
+          <Sections />
+        </>
+        :
+        <div className="largescreens">
+          This application is only available on large screens
+        </div>
+      }
       {/* <button onClick={() => connectWallet(setSigner)}>Connect Button</button> */}
     </div>
   );
